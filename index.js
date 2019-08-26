10c380d763ae79ec90ebad27ffd31f25ed33bad2
ruben <r.villca.fernandez@gmail.com>
2019-08-26 03:08:28 -0400

    fix

--- index.js
+++ index.js
@@ -29,10 +29,11 @@ app.use(bodyParser.json());
 
 app.engine('html', require('ejs').renderFile);
 
+app.set('port',process.env.PORT||3000);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/public'));
 let router = express.Router();
-//router.use(express.static(path.join(__dirname, '/public')));
+router.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', indexRouter);
 app.set('view engine', 'ejs');
@@ -55,7 +56,6 @@ app.get('*', (req, res) => {
   res.redirect('/login');
 });
 
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'),  () => {
+  console.log('Example app listening on port:'+ app.get('port')+'!');
 });
